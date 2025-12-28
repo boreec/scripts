a20c5cd78bf93ec89c5d5d57bafa800ea6063fe2
Cyprien Boree <51238594+boreec@users.noreply.github.com>
2025-12-28 10:07:23 +0000

    chore: update setup-arch-dependencies

--- setup-arch-dependencies.sh
+++ setup-arch-dependencies.sh
@@ -3,11 +3,12 @@
 
 set -euo pipefail
 
-pacman -S acpica brightnessctl biome blueman bluez clang curl dmidecode direnv \
-  dolphin efibootmgr efivar fd firefox fish fzf fwupd git github-cli keepassxc \
-  kitty go hyprland jq less lua luarocks ly lz4 mako man-db mise most \
-  noto-fonts-cjk npm nvim perl prettier python python-pip rofi rust-analyzer \
-  starship stylua tokei tmux tree ttf-jetbrains-mono-nerd uv vlc \
-  vlc-plugins-all waybar wget wl-clipboard zoxide
+pacman -S acpica brightnessctl bind-tools biome blueman bluez clang curl \
+  dmidecode direnv dolphin efibootmgr efivar fd firefox fish fzf fwupd git \
+  github-cli keepassxc kitty go hyprland jq less lua luarocks ly lz4 mako \
+  man-db mise most noto-fonts-cjk npm nvim perl prettier python python-pip \
+  rofi rust-analyzer starship stubby stylua tokei tmux tree \
+  ttf-jetbrains-mono-nerd uv vlc vlc-plugins-all waybar wget wl-clipboard \
+  zoxide
 
 cargo install --locked tree-sitter-cli
