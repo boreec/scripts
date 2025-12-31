318df0cf54d37a0d23900c51f6e9799518d7aca1
Cyprien Boree <51238594+boreec@users.noreply.github.com>
2025-12-31 19:24:15 +0000

    feat: add make to arch dependencies

--- setup-arch-dependencies.sh
+++ setup-arch-dependencies.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 pacman -S acpica brightnessctl bind-tools biome blueman bluez clang curl \
   dmidecode direnv dolphin efibootmgr efivar fd firefox firewalld fish fzf \
   fwupd git github-cli keepassxc kitty go hyprland jq less lua luarocks ly \
-  lz4 mako man-db mise most networkmanager network-manager-applet \
+  lz4 make mako man-db mise most networkmanager network-manager-applet \
   noto-fonts-cjk npm nvim pavucontrol perl pipewire pipewire-alsa pkg-config \
   pipewire-jack pipewire-pulse prettier python python-pip reflector rofi \
   rust-analyzer snap-pac snapper starship stubby stylua swayosd timeshift \
