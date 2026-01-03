a35e994a076cb62f7dbb312d31de3b97212f1888
Cyprien Boree <51238594+boreec@users.noreply.github.com>
2026-01-03 16:14:54 +0000

    feat: add archlinux-xdg-menu to arch dependencies

--- setup-arch-dependencies.sh
+++ setup-arch-dependencies.sh
@@ -3,15 +3,15 @@
 
 set -euo pipefail
 
-pacman -S acpica brightnessctl bind-tools biome blueman bluez bottom clang \
-  curl dmidecode direnv dolphin efibootmgr efivar fd firefox firewalld fish \
-  fzf fwupd git github-cli keepassxc kitty go htop hyperfine hyprland jq less \
-  lua luarocks ly lz4 make mako man-db mise most ncdu networkmanager \
-  network-manager-applet noto-fonts-cjk npm nvim pavucontrol perl pipewire \
-  pipewire-alsa pkg-config pipewire-jack pipewire-pulse prettier python \
-  python-pip reflector rofi rust-analyzer snap-pac snapper starship stubby \
-  stylua swayimg swayosd timeshift tldr tmux tokei tree \
-  ttf-jetbrains-mono-nerd upx uv vlc vlc-plugins-all waybar wget \
+pacman -S acpica archlinux-xdg-menu brightnessctl bind-tools biome blueman \
+  bluez bottom clang curl dmidecode direnv dolphin efibootmgr efivar fd \
+  firefox firewalld fish fzf fwupd git github-cli keepassxc kitty go htop \
+  hyperfine hyprland jq less lua luarocks ly lz4 make mako man-db mise most \
+  ncdu networkmanager network-manager-applet noto-fonts-cjk npm nvim \
+  pavucontrol perl pipewire pipewire-alsa pkg-config pipewire-jack \
+  pipewire-pulse prettier python python-pip reflector rofi rust-analyzer \
+  snap-pac snapper starship stubby stylua swayimg swayosd timeshift tldr tmux \
+  tokei tree ttf-jetbrains-mono-nerd upx uv vlc vlc-plugins-all waybar wget \
   wireguard-tools wireplumber wl-clipboard zig zoxide
 
 cargo install --locked tree-sitter-cli
